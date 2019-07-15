bacccccdb1297524d336920a43a02d55064faec4
Oscar Miniet <ominiet96@hotmail.com>
2019-07-15 17:54:53 -0500

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+let express = require('express'),
+    path    = require('path');
+
+const PORT = process.env.PORT || 8080
+
+let app = express();
+
+app.use(express.static(__dirname + '/dist'));
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening to ${PORT}....`)
+    console.log('Press Ctrl+C to quit.')
+});
