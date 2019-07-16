67a90b31b033e4555e2790ca96b541b2524774bf
Oscar Miniet <ominiet96@hotmail.com>
2019-07-16 08:04:36 -0500

    Jose has joined the battle

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080
 
 let app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/public'));
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
